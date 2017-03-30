e6a0c77aacbf149bfc1ef86ae4d7864e2a6d936f
remusd <remusdragomir9@gmail.com>
2017-03-30 14:36:03 +0300

    Update sample_installer.sh

--- sample_installer.sh
+++ sample_installer.sh
@@ -16,14 +16,15 @@ REMOTE=https://raw.github.com/altercation/archblocks/master
 
 # CONFIG -----------------------------------------------------------------
 
-HOSTNAME=tau
-USERNAME=es
+HOSTNAME=workstation1
+USERNAME=student1
 USERSHELL=/bin/bash
 FONT=Lat2-Terminus16
 FONT_MAP=8859-1_to_uni
 LANGUAGE=en_US.UTF-8
 KEYMAP=us
-TIMEZONE=US/Pacific
+#TIMEZONE=US/Pacific
+TIMEZONE=EU/Bucharest
 MODULES="dm_mod dm_crypt aes_x86_64 ext2 ext4 vfat intel_agp drm i915"
 HOOKS="base udev autodetect pata scsi sata usb usbinput consolefont encrypt filesystems fsck shutdown"
 KERNEL_PARAMS="quiet i915.i915_enable_rc6=1 i915.i915_enable_fbc=1 i915.lvds_downclock=1"
@@ -48,7 +49,7 @@ SENSORS=common/sensors_default
 XORG="xorg/xorg_default xorg/xorg_fonts_infinality xorg/xorg_wacom xorg/xorg_synaptics xorg/mesa_dri"
 VIDEO=video/video_intel
 DESKTOP=xorg/desktop_xmonad_minimal
-HARDWARE=hardware/laptop/lenovo_thinkpad_x220
+HARDWARE=hardware/laptop/dell_latitude_e7450
 APPSETS="appsets/cli_hardcore appsets/vim_basics appsets/mutt_basics appsets/git_basics appsets/server_utils appsets/chromium_basics"
 
 # EXTRA PACKAGES ---------------------------------------------------------
